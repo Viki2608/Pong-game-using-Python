1f0915358c9a50256d386bce9f9cb7f17d0c77b6
Viki2608 <vignesh26.manoharan@gmail.com>
2021-08-02 20:33:23 +0530

    Pong game using Python and OOPS Concept

---
+++ paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+    
+
+
+
+
+    def go_up(self):
+        new_xcor = self.xcor()
+        new_ycor = self.ycor() + 20
+        self.goto(new_xcor, new_ycor)
+
+    def go_down(self):
+        new_xcor = self.xcor()
+        new_ycor = self.ycor() - 20
+        self.goto(new_xcor, new_ycor)
+
